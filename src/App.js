f5579fb9255ec1511f8f05043d8c736561ff770c
Kevin <dokkevin@gmail.com>
2019-04-28 18:21:44 +0900

    Home Link Created
    customizable for color through props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Uc from './Uc.js';
+import HomeLink from './HomeLink.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <HomeLink color="#be315b"/>
           <Uc />
       </header>
     </div>
