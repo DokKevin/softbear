64b181774954dad7cbb5013b198180278a3dfa46
Kevin Kauffman <DokKevin@users.noreply.github.com>
2019-05-04 16:36:15 +0900

    Revert "Soft page"

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
+import Uc from './Uc.js';
 import HomeLink from './HomeLink.js';
-import Langs from './Langs.js';
-import Soft from './Soft.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <HomeLink color="#be315b"/>
-          <div id="intro">
-            <div class="buffer">Buffer</div>
-            <h1>Hello there...</h1>
-            <div class="buffer">Buffer</div>
-            <h2>My name is Kevin.</h2>
-            <div class="buffer">Buffer</div>
-            <h3>I do these things:</h3>
-          </div>
-          <div class="homeLinkWrapper">
-            <div href="" class="homeLink" onClick={renderLangs}>Learn Languages</div>
-          </div>
-          <div class="homeLinkWrapper" onClick={renderSoft}>
-            <div href="" class="homeLink">Develop Software</div>
-          </div>
+          <Uc />
       </header>
     </div>
   );
-
-  function renderLangs() {
-      ReactDOM.render(<Langs />, document.getElementById('root'));
-  }
-
-  function renderSoft() {
-      ReactDOM.render(<Soft />, document.getElementById('root'));
-  }
 }
 
 export default App;
