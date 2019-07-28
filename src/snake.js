b4ab803d1171261d50389f940bc223a2316471d1
DokKevin <dokkevin@gmail.com>
2019-07-28 11:24:53 -0400

    Updated Snake to latest version

--- src/snake.js
+++ src/snake.js
@@ -8,6 +8,7 @@ class Snake extends React.Component {
             player: {
                 top: '380px',
                 left: '380px',
+                body: [],
                 direction: "north"
             },
             treat: {
@@ -15,8 +16,12 @@ class Snake extends React.Component {
                 left: '380px'
             },
             didStart: "Start",
+            // TODO: persist game modes' individual high score when switching game modes
             score: 0,
-            highScore: 0
+            highScore: 0,
+            easyButtID: "notSelected",
+            hardButtID: "selected",
+            mode: 1
         };
     }
 
@@ -34,6 +39,14 @@ class Snake extends React.Component {
     async updatePlayerPos(){
         let tempTop = parseInt(this.state.player.top.substring(0, this.state.player.top.length - 1));
         let tempLeft = parseInt(this.state.player.left.substring(0, this.state.player.left.length - 1));
+        let tempBody = this.state.player.body;
+
+        if(this.state.mode && this.state.score > 0){
+            let tempSeg = tempBody.pop();
+            tempBody.unshift(tempSeg);
+            tempBody[0][0] = this.state.player.top;
+            tempBody[0][1] = this.state.player.left;
+        } // else do nothing
 
         if(this.state.player.direction === "west"){
             tempLeft = tempLeft - 20;
@@ -41,6 +54,7 @@ class Snake extends React.Component {
                 player: {
                     top: tempTop.toString() + "px",
                     left: tempLeft.toString() + "px",
+                    body: tempBody,
                     direction: "west"
                 }
             });
@@ -50,6 +64,7 @@ class Snake extends React.Component {
                 player: {
                     top: tempTop.toString() + "px",
                     left: tempLeft.toString() + "px",
+                    body: tempBody,
                     direction: "north"
                 }
             });
@@ -59,6 +74,7 @@ class Snake extends React.Component {
                 player: {
                     top: tempTop.toString() + "px",
                     left: tempLeft.toString() + "px",
+                    body: tempBody,
                     direction: "east"
                 }
             });
@@ -68,6 +84,7 @@ class Snake extends React.Component {
                 player: {
                     top: tempTop.toString() + "px",
                     left: tempLeft.toString() + "px",
+                    body: tempBody,
                     direction: "south"
                 }
             });
@@ -86,6 +103,25 @@ class Snake extends React.Component {
         } else {
             return await false;
         }
+    }
+
+    addTailSeg = () => {
+        if(this.state.mode){
+            let tempPlayerTop = parseInt(this.state.player.top.substring(0, this.state.player.top.length - 1));
+            tempPlayerTop = tempPlayerTop - 1;
+
+            let tempBody = this.state.player.body;
+            tempBody.push([tempPlayerTop.toString() + "px", this.state.player.left]);
+
+            this.setState({
+                player: {
+                    top: this.state.player.top,
+                    left: this.state.player.left,
+                    body: tempBody,
+                    direction: this.state.player.direction
+                }
+            });
+        } // else do nothing
     }
 
     gotPoint = () => {
@@ -107,6 +143,8 @@ class Snake extends React.Component {
             score: newScore,
             highScore: newHigh
         });
+
+        this.addTailSeg();
     }
 
     async checkDeath(){
@@ -116,6 +154,16 @@ class Snake extends React.Component {
         if(tempTop < 0 || tempTop === 760 || tempLeft < 0 || tempLeft === 760){
             this.resetGame();
         }
+
+        if(this.state.mode){
+            for(let i = 0; i < this.state.player.body.length; i++){
+                let tempSegTop = parseInt(this.state.player.body[i][0].substring(0, this.state.player.body[i][0].length - 1));
+                let tempSegLeft = parseInt(this.state.player.body[i][1].substring(0, this.state.player.body[i][1].length - 1));
+                if(tempTop === tempSegTop && tempLeft === tempSegLeft){
+                    this.resetGame();
+                }
+            }
+        } // else do nothing
     }
 
     toggleGame = () => {
@@ -131,6 +179,8 @@ class Snake extends React.Component {
                 100
             );
         } else {
+            document.getElementById("snakeContainer").focus();
+
             this.setState({
                 didStart: "Start"
             });
@@ -141,7 +191,7 @@ class Snake extends React.Component {
 
     resetGame = () => {
         document.getElementById("snakeContainer").focus();
-        
+
         if(this.state.didStart === "Stop"){
             this.toggleGame();
         } // else do nothing
@@ -150,6 +200,7 @@ class Snake extends React.Component {
             player: {
                 top: '380px',
                 left: '380px',
+                body: [],
                 direction: "north"
             },
             score: 0
@@ -165,40 +216,58 @@ class Snake extends React.Component {
                 this.toggleGame();
                 break;
             case 37:
-                this.setState({
-                    player: {
-                        top: this.state.player.top,
-                        left: this.state.player.left,
-                        direction: "west"
-                    }
-                });
+                if(this.state.player.direction != "east"){
+                    this.setState({
+                        player: {
+                            top: this.state.player.top,
+                            body: this.state.player.body,
+                            left: this.state.player.left,
+                            direction: "west"
+                        }
+                    });
+                } // else do nothing
                 break;
             case 38:
-                this.setState({
-                    player: {
-                        top: this.state.player.top,
-                        left: this.state.player.left,
-                        direction: "north"
-                    }
-                });
+                if(this.state.player.direction != "south"){
+                    this.setState({
+                        player: {
+                            top: this.state.player.top,
+                            body: this.state.player.body,
+                            left: this.state.player.left,
+                            direction: "north"
+                        }
+                    });
+                } // else do nothing
                 break;
             case 39:
-                this.setState({
-                    player: {
-                        top: this.state.player.top,
-                        left: this.state.player.left,
-                        direction: "east"
-                    }
-                });
+                if(this.state.player.direction != "west"){
+                    this.setState({
+                        player: {
+                            top: this.state.player.top,
+                            left: this.state.player.left,
+                            body: this.state.player.body,
+                            direction: "east"
+                        }
+                    });
+                } // else do nothing
                 break;
             case 40:
-                this.setState({
-                    player: {
-                        top: this.state.player.top,
-                        left: this.state.player.left,
-                        direction: "south"
-                    }
-                });
+                if(this.state.player.direction != "north"){
+                    this.setState({
+                        player: {
+                            top: this.state.player.top,
+                            left: this.state.player.left,
+                            body: this.state.player.body,
+                            direction: "south"
+                        }
+                    });
+                } // else do nothing
+                break;
+            case 69:
+                this.toggleMode(0);
+                break;
+            case 72:
+                this.toggleMode(1);
                 break;
             case 82:
                 this.resetGame();
@@ -218,18 +287,58 @@ class Snake extends React.Component {
         document.getElementById("snakeContainer").focus();
     }
 
+    toggleMode = (nMode) => {
+        if(this.state.didStart === "Stop"){
+            this.resetGame();
+        } // else do nothing
+
+        // Temporarily hard reset on game mode switch
+        this.setState({
+            score: 0,
+            highScore: 0
+        });
+
+        let tempEasy = "selected";
+        let tempHard = "notSelected";
+
+        if(nMode === 1){
+            tempEasy = "notSelected";
+            tempHard = "selected";
+        } // else stay as assigned above if statement
+
+        this.setState({
+            easyButtID: tempEasy,
+            hardButtID: tempHard,
+            mode: nMode
+        });
+
+        document.getElementById("snakeContainer").focus();
+    }
+
+    //TODO: Make game size reactive
+    //TODO: Popup list of keyboard shortcuts
+    //TODO: Make game playable on mobile
+
     render() {
         return (
             <div className="background-wrapper">
                 <div className="exit" onClick={this.handleDismiss}>X</div>
                 <div className="buttonWrapper">
-                    <span className="gameButton" onClick={this.toggleGame}>{this.state.didStart} Game</span>
-                    <span className="gameButton" onClick={this.resetGame}>Reset Game</span>
+                    <span className="diffButton leftButt" id={this.state.hardButtID} onClick={() => this.toggleMode(1)}>Hard Mode</span>
+                    <span className="diffButton rightButt" id={this.state.easyButtID} onClick={() => this.toggleMode(0)}>Easy Mode</span>
+
+                    <span className="gameButton leftButt" id={this.state.didStart} onClick={this.toggleGame}>{this.state.didStart} Game</span>
+                    <span className="gameButton rightButt" onClick={this.resetGame}>Reset Game</span>
                 </div>
                 <div id="snakeContainer" tabIndex="1" onKeyDown={(e) => this.handleKeyPress(e)}>
                     <div className="score">{this.state.score}</div>
                     <div className="highScore">{this.state.highScore}</div>
                     <span className="snakePlayer" style={this.state.player}></span>
+                    <span>
+                        {
+                            this.state.player.body.map(el => <span className="snakePlayer" style={{top: el[0], left: el[1]}}></span>)
+                        }
+                    </span>
                     <span className="treat" style={this.state.treat}></span>
                 </div>
             </div>
